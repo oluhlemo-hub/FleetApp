531905151123b68272213a91c4aa8db2b3b66509
Suli Mohamed <suli@different.co.za>
2026-05-18 19:10:39 +0200

    Fix login bypass, add vehicle request dynamic Resend OTP pipeline

--- obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db357fa66fc628269209de1c7e9642c7edb0478e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79d71ded9404c81b5aa921178af101497928865e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
