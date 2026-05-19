7dbec4f4bf944e8891f8de949cbadfaa9525b379
Suli Mohamed <suli@different.co.za>
2026-05-19 18:19:46 +0200

    Auto-deploy: updated obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs,obj/Debug/net8.0/FleetManagement.AssemblyInfoInputs.cache,obj/Debug/net8.0/FleetManagement.csproj.CoreCompileInputs.cache,obj/Debug/net8.0/FleetManagement.sourcelink.json [2026-05-19 18:19]

--- obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd9be6d72d88a74c08767f52f0fffb966671468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9249949c4f711ab0ff8f83b2251080392cb933fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
