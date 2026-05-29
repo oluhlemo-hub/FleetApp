b1806be7b0ad491062a5ffcd5ec4c268a49f0a2d
Suli Mohamed <suli@different.co.za>
2026-05-29 11:25:44 +0200

    Expose email error for debugging

--- obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c47818741b4d68e40a2baccdf88ca3471b391f2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d29d92e5d6d5308ead7c0b212d564c3d495756")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
