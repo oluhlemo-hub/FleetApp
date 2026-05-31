8b4e9086d0cb3b29aa3f786c60396ee960db0763
Suli Mohamed <suli@different.co.za>
2026-05-31 08:29:22 +0200

    Fix password reset redirect to change-password page

--- obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0a1a559f60db0bed56140b6bdf791acdae8a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f936d1ef13527e78e79f7e6b83958c8f250f66bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
