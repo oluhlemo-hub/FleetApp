3b21199e53e85a492e06f3c20a9581c968802c94
Fleet Portal <fleetporta26@gmail.com>
2026-06-06 03:25:14 +0200

    Redesign: gradient vehicle card, fix dark/light mode, remove last driver

--- obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e335708226a2b74c02afac547df23d3cd91a227f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f474775266bf2c0bed6870e9869c77e16c6b9882")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
