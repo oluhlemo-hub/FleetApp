35b690d13a2330c535c93361289752a913a1c9be
Fleet Portal <fleetporta26@gmail.com>
2026-06-09 10:35:17 +0200

    Fix: all admin tables themed - vehicles, drivers, faults, fuel logs, documents

--- obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/FleetManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791292648efb8f55d8f21a21691273324aecb4b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a623fdf1e5a0781d99f7084ace5b5840830416")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
